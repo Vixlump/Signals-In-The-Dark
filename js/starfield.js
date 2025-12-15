06f21297181c20deba5071352dc1efda1f70908d
riyadaiya <127396618+riyadaiya@users.noreply.github.com>
2025-12-15 05:42:43 +0000

    storytelling polish

--- js/starfield.js
+++ js/starfield.js
@@ -8,7 +8,7 @@ function createStarfield() {
     const height = window.innerHeight;
     
     const stars = [];
-    const starCount = 400;
+    const starCount = 200;
     
     for (let i = 0; i < starCount; i++) {
         stars.push({
